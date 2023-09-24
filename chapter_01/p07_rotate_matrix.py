ad3e73d881bbe0eddad1c157525968d96ca7c452
Georgios Etsias <etsiasg@hotmail.gr>
2023-09-24 15:59:22 +0100

    Update p07_rotate_matrix.py

--- chapter_01/p07_rotate_matrix.py
+++ chapter_01/p07_rotate_matrix.py
@@ -2,6 +2,7 @@
 import unittest
 from copy import deepcopy
 
+# GE: both rotate_matrix and rotate_matrix_double_swap have O(N^2), the double swap has an additional O(N) underneath it, that is why it might be considered less optimal.
 
 def rotate_matrix(matrix):
     """rotates a matrix 90 degrees clockwise"""
@@ -16,7 +17,7 @@ def rotate_matrix(matrix):
             matrix[layer][i] = matrix[-i - 1][layer]
 
             # bottom -> left
-            matrix[-i - 1][layer] = matrix[-layer - 1][-i - 1]
+            matrix[-i - 1][layer] = matrix[-layer - 1][-i - 1]  # GE: it uses negative indices this is not correct, it should be re-written I think.
 
             # right -> bottom
             matrix[-layer - 1][-i - 1] = matrix[i][-layer - 1]
@@ -42,7 +43,23 @@ def rotate_matrix_double_swap(matrix):
     return matrix
 
 
-def rotate_matrix_pythonic(matrix):
+# chat GPT solution, transpose matrix then reverse its rows GE -> I believe this solution is superior to the one in the book! -> it is the same with rotate_matrix_double_swap here
+def rotate_matrix_in_place(matrix):
+    n = len(matrix)
+
+    # Step 1: Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Step 2: Reverse the rows (for a 90-degree counterclockwise rotation)
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
+
+    return matrix
+
+
+def rotate_matrix_pythonic(matrix):           # GE: this solution does NOT take into account the matrix rotation 'in place' requirement. Instead it creates a new matrix. Wrong.
     """rotates a matrix 90 degrees clockwise"""
     n = len(matrix)
     result = [[0] * n for i in range(n)]  # empty list of 0s
